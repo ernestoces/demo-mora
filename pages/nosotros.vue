6371b94a9b430444801ee2c3924fb95b1e1da294
vr1 <hivr1eva@gmail.com>
2024-05-22 03:00:37 -0500

    Update nosotros.vue

--- pages/nosotros.vue
+++ pages/nosotros.vue
@@ -7,21 +7,18 @@
             <template #header>
                 <div class="flex flex-col gap-[24px] items-center justify-center xl:pt-[88px] pt-[56px]">
                     <h2 class="font-montserrat font-bold text-[33px] leading-[40px] text-dark xl:text-left text-center">
-                        ¡Únete a la
-                        Automatización!
+                        ¡Únete a la Automatización!
                     </h2>
-                    <p class="font-raleway font-normal text-[18px] leading-[21px] xl:text-left text-center">Estas son
-                        algunas de las compañías
-                        que
-                        confían en nuestros servicios.</p>
+                    <p class="font-raleway font-normal text-[18px] leading-[21px] xl:text-left text-center text-dark">
+                        Estas son
+                        algunas de las compañías que confían en nuestros servicios.</p>
                 </div>
             </template>
             <template #footer>
                 <div>
                     <button
                         class="font-raleway font-semibold text-base leading-[27px] w-full xl:w-auto -tracking-[1%] text-mora py-[8px] px-[22px] rounded-[3px] border-2 border-mora mx-auto block">Conoce
-                        más
-                        de nuestros servicios</button>
+                        más de nuestros servicios</button>
                 </div>
             </template>
         </Brands>
