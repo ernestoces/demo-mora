fdb055b90f969abf107d6ad256547a06f1dc5e21
vr1 <hivr1eva@gmail.com>
2024-05-27 21:52:34 -0500

    update office address

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -10,7 +10,7 @@ const branches = [
         country: "Perú",
         email: "contacto.peru@automationanywhere.com",
         phone: "+51 987 654 321",
-        location: "Av. Rivera Navarrete 395 of. 1692 - San Isidro, Perú"
+        location: "Av. Rivera Navarrete 395 of. 1602 - San Isidro, Perú"
     }
 ]
 
